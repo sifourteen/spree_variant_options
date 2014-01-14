a819f74ac07150d02c7d9fd5d80c693bce8ae9cf
Viacheslav Boretskyi <vboretskiy@gmail.com>
2014-01-14 15:26:08 +0200

    fix currency view

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -3,12 +3,12 @@ Spree::Variant.class_eval do
   include ActionView::Helpers::NumberHelper
 
   def to_hash
-    actual_price  = self.price
+    #actual_price  = self.price
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
     {
       :id    => self.id,
       :in_stock => self.in_stock?,
-      :price => number_to_currency(actual_price)
+      :price => self.display_price
     }
   end
 
