838569110b787e55d4674b410f12b3c60bb7d427
Garrett Jacobson <garrett.jacobson@gmail.com>
2014-03-03 00:26:02 -0800

    Initial prototype of new dropdown variant option

--- app/assets/javascripts/store/spree_variant_options.js
+++ app/assets/javascripts/store/spree_variant_options.js
@@ -1,4 +1,7 @@
 //= require spree/frontend
 
+//= require chosen/chosen.jquery
+
 //= require store/product_variant_options
 //= require store/variant_options
+//= require store/dropdown_variant_options
