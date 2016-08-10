31cb2776f7771cba9d7dc5ff254c92a34a7f24fb
Devon Anderson <devonxanderson@gmail.com>
2016-08-09 20:19:27 -0700

    Moved all asset files into spree path

--- lib/generators/spree_variant_options/install_generator.rb
+++ lib/generators/spree_variant_options/install_generator.rb
@@ -9,12 +9,12 @@ module SpreeVariantOptions
       end
 
       def add_javascripts
-        append_file "app/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_variant_options\n"
+        append_file "app/assets/javascripts/spree/frontend/all.js", "\n//= require spree/frontend/spree_variant_options\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/spree/frontend/all.css", "*= require spree/frontend/spree_variant_options\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/spree/backend/all.css", "*= require spree/backend/spree_variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/spree/frontend/all.css", "\n*= require spree/frontend/spree_variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/spree/backend/all.css", "\n*= require spree/backend/spree_variant_options\n", :before => /\*\//, :verbose => true
       end
 
     end
