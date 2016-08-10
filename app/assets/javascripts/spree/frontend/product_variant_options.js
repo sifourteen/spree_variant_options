cc5c64671415a574467fb09f3e7f61ec08f100a7
Devon Anderson <devonxanderson@gmail.com>
2016-08-09 23:27:04 -0700

    Changing html instead of text, as missing translation results in a span

--- app/assets/javascripts/spree/frontend/product_variant_options.js
+++ app/assets/javascripts/spree/frontend/product_variant_options.js
@@ -12,7 +12,6 @@ var show_variant_images = function(variants_ids) {
     // if currently selected thumb does not belong to current variant, nor to common images,
     // hide it and select the first available thumb instead.
     if(!currentThumb.hasClass('vtmb-' + variant_id)) {
-      //var thumb = $($('ul.thumbnails li:visible').eq(0));
       var thumb = $($("ul.thumbnails li.vtmb-" + variant_id + ":first").eq(0));
       if (thumb.length == 0) {
         thumb = $($('ul.thumbnails li:visible').eq(0));
