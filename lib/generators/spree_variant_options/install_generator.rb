fbfd0f79ea04b88e077ffad045475b166db91838
Devon Anderson <devonxanderson@gmail.com>
2016-08-09 17:51:54 -0700

    updating injection points

--- lib/generators/spree_variant_options/install_generator.rb
+++ lib/generators/spree_variant_options/install_generator.rb
@@ -9,12 +9,12 @@ module SpreeVariantOptions
       end
 
       def add_javascripts
-        append_file "app/assets/javascripts/frontend/all.js", "//= require store/spree_variant_options\n"
+        append_file "app/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_variant_options\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/frontend/all.css", "*= require store/spree_variant_options\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/backend/all.css", "*= require admin/spree_variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/spree/frontend/all.css", "*= require spree/frontend/spree_variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/spree/backend/all.css", "*= require spree/backend/spree_variant_options\n", :before => /\*\//, :verbose => true
       end
 
     end
